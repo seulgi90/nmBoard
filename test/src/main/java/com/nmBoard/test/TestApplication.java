99d749417cb4ed2f74a88261ca133adeec52b625
seulgi90 <seulgi6380@gmail.com>
2023-02-20 22:07:17 +0900

    test

---
+++ test/src/main/java/com/nmBoard/test/TestApplication.java
@@ -0,0 +1,13 @@
+package com.nmBoard.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
